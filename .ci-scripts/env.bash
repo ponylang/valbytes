8a18862ff824da69fb05c9194007e2b234c0cb24
Matthias Wahl <matthiaswahl@m7w3.de>
2020-05-06 21:37:49 +0200

    move to github actions

--- .ci-scripts/env.bash
+++
@@ -1,8 +0,0 @@
-REPO_OWNER="mfelsche"
-REPO_NAME="pony-valbytes"
-GITHUB_USER="mfelsche"
-
-# Who we are for git
-git config --global user.email "matthiaswahl@m7w3.de"
-git config --global user.name "Maatthias Wahl"
-git config --global push.default simple
---
+++ .ci-scripts/release/config.bash
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+export LIBRARY_NAME="valbytes"
+export DOCS_BUILD_DIR="build/valbytes-docs"
